03fedb949a89e0fafc916bf436fb3acb201b2928
depozzyx <mosentsowivan@gmail.com>
2020-06-09 19:04:00 +0300

    xxx

--- server/server.js
+++ server/server.js
@@ -7,15 +7,15 @@ var app = express();
 var cors = require('cors');
 const bodyParser = require('body-parser');
 
-var path = require('path');
-var serveStatic = require('serve-static');
-app.use(serveStatic("../"));
-var port = process.env.PORT || 5000;
-var hostname = '127.0.0.1';
-
-app.listen(port, hostname, () => {
-   console.log(`Server running at http://${hostname}:${port}/`);
- });
+// var path = require('path');
+// var serveStatic = require('serve-static');
+// app.use(serveStatic("../"));
+// var port = process.env.PORT || 5000;
+// var hostname = '127.0.0.1';
+//
+// app.listen(port, hostname, () => {
+//    console.log(`Server running at http://${hostname}:${port}/`);
+//  });
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
